b997d637ea9224653de69433191c2a1dfe59c1cb
Xiaohang Su <sxhdragon@gmail.com>
2017-04-22 00:56:53 -0400

    movie serve and backend support movie

--- app/index.js
+++ app/index.js
@@ -1,2 +1,20 @@
 require('./../style/scss/style.scss');
 import Vue from './vue';
+
+let videoEle = document.getElementsByTagName('video')[0];
+videoEle.addEventListener('canplay', ()=>{
+    console.log('I can Play');
+    videoEle.play();
+});
+
+let playButton = document.getElementById('play');
+
+playButton.addEventListener('click', ()=>{
+    videoEle.currentTime = 10;
+});
+
+let pauseButton = document.getElementById('pause');
+
+pauseButton.addEventListener('click', ()=>{
+    videoEle.pause();
+})
--- routes/index.js
+++ routes/index.js
@@ -19,15 +19,42 @@ router.get('/', (ctx, next)=> {
     });
 });
 
-let start = 0;
+let START_TIME = 0;
 router.get('/*.mp4', (ctx, next)=> {
-    if (start === 0) start = (new Date()).getTime();
-        return send(ctx, '/public/movie.mp4');
+    if (START_TIME === 0) START_TIME = (new Date()).getTime();
+    console.log(__dirname + '/../public' + ctx.req.url);
+    let path = __dirname + '/../public' + ctx.req.url;
+    let stat = fs.statSync(path);
+    let total= stat.size;
+    let start, end;
+    if (ctx.req.headers.range){
+        let range = ctx.req.headers.range;
+        let parts = range.replace(/bytes=/, '').split('-');
+        start = parts[0];
+        end   = parts[1];
+        start = parseInt(start);
+        end   = end ? parseInt(end) : total - 1;
+        var chunksize = (end - start) + 1;
+        console.log('Video Range: ' + start + ' - ' + end + ' = ' + chunksize);
+        let file = fs.createReadStream(path, {start: start, end: end});
+        ctx.res.writeHead(206, {
+            'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4'});
+        ctx.body = file.pipe(ctx.res);
+    } else {
+        console.log('All Video File: ' + total);
+        ctx.res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+        ctx.body = fs.createReadStream(path).pipe(ctx.res);
+    }
+
+
 });
 
 router.get('/offset', (ctx, next)=>{
     ctx.body = {
-        offset: (new Date()).getTime() - start
+        offset: (new Date()).getTime() - START_TIME
     };
 });
 
